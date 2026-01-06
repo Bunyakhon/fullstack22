4b6283b0c66bbf4fa25cca238d0c3522fe819f69
Bunyakhon <growmos@gmail.com>
2026-01-06 08:34:12 +0700

    terst

--- problem100/sp1.js
+++ problem100/sp1.js
@@ -22,3 +22,4 @@ function subtrack_target(num_list, target) {
 }
 console.log(sum_target([1,2,3,4,5,6,7,8,9],15))
 console.log(subtrack_target([1,2,3,4,5,6,7,8,9],5))
+console.log(subtrack_target([1,2,3,4,5,6,7,8,9],7))
