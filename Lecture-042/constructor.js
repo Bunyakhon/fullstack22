0c65d7e94fd356d30da80d6a585ccd3673dca127
Bunyakhon <growmos@gmail.com>
2026-01-08 13:34:02 +0700

    mid

---
+++ Lecture-042/constructor.js
@@ -0,0 +1,9 @@
+class Song{
+    constructor(titel,artist){
+        this.title = titel;
+        this.artist = artist;
+    }
+}
+const mySong = new Song("Behemain Thasody","Queen");
+console.log(mySong.title);
+console.log(mySong.artist);
